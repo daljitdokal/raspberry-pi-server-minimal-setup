5beb1fc995896598b6aeb4c318b444223f61d6f1
Daljit Singh <daljit.dokal@yahoo.co.nz>
2022-01-23 14:29:17 +1300

    Documentation updated

--- post-install/post-install-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run awx custom configration playbook
-ansible-playbook playbook/awx/playbook-config-awx.yml -i inventory/geeky-server-build/hosts.ini  --ask-vault-password
--- post-install/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-ansible-galaxy collection install awx.awx
-
-# Run anible master playbook
-ansible-playbook site.yml -i inventory/geeky-server-build/hosts.ini
